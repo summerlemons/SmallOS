e029f4ca6a0ff042769740466ba850a2b497e48c
Martics <2032153705@qq.com>
2026-01-10 19:47:57 +0800

    change: 删除 build.sh 文件

--- build.sh
+++
@@ -1,8 +0,0 @@
-rm -rf ./target
-mkdir target
-nasm boot.asm -o target/boot.o
-nasm setup.asm -o target/setup.o
-bximage -q -hd=16 -func=create -sectsize=512 -imgmode=flat target/hd.img
-dd if=target/boot.o of=target/hd.img bs=512 count=1 seek=0 conv=notrunc
-dd if=target/setup.o of=target/hd.img bs=512 count=2 seek=1 conv=notrunc
-bochs -q -f bochsrc
