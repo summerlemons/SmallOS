7f34cb73e55834d08d8b811f6d5ec4b4d358d45c
Martics <2032153705@qq.com>
2026-01-12 18:29:25 +0800

    features: boot.asm 加载 setup.asm 的逻辑,从 int 0x13 中断形式读取, 改为读取硬盘端口 0x1f0 ~ 0x1f7 形式读取. 目前只实现读取一个扇区

--- boot.asm
+++ boot.asm
@@ -20,18 +20,76 @@ _start:
     call print
 
 
-    mov ch, 0  ; 柱面：0
-    mov dh, 0  ; 磁头：0
-    mov cl, 2  ; 扇区：2
-    mov bx, BOOT_MAIN_ADDR  ; 加载到 ES:BX = 0x0000:0x500
+    mov ecx, 1 ; 从硬盘哪个扇区开始读。这里尤其要注意，CHS 方式读取扇区从 1 开始，但是 LBA 模式从 0 开始！！
+    mov bl, 1  ; 读取的扇区数量
+    
+    ; 0x1f2 8bit 指定读取或写入的扇区数
+    mov dx, 0x1f2
+    mov al, bl
+    out dx, al
 
-    mov ah, 0x02 ; 功能：读扇区
-    mov al, 2    ; 扇区数：2   表示我要读几个扇区， 1 个扇区 = 512 字节
-    mov dl, 0x80 ; 驱动器：0x80=第一个硬盘；0x81=第二个硬盘， 如果是软盘，则驱动器为0x00，0x01编号
-    int 0x13
+    ; 0x1f3 8bit iba地址的低八位 0-7
+    inc dx
+    mov al, cl
+    out dx, al
 
-    ; 这里 int 0x13 读取磁盘出错, 就会修改 FLAGS 寄存器的 CF 位 =1，则 jc 跳转命令生效 
-    jc .read_disk_error
+    ; 0x1f4 8bitiba地址的中八位 8-15
+    inc dx
+    mov al, ch
+    out dx, al
+
+    ; 0x1f5 8bitiba地址的高八位 16-23
+    inc dx
+    shr ecx, 16
+    mov al, cl
+    out dx, al
+
+    ; 0x1f6 8bit
+    ; 0-3 位iba地址的24-27
+    ; 4 0表示主盘 1表示从盘
+    ; 5、7位固定为1
+    ; 6 0表示CHS模式，1表示LAB模式
+    inc dx
+    mov al, ch  ; 0-3 位iba地址的24-27
+    and al, 0b0000_1111  ; 这里把 4-7 的位清零，防止有数据影响
+    or al, 0b1110_0000   ; 这里设置 5,6,7 位为1, 5,7 固定就是1, 6 设置 1 表示 LBA 读取
+    out dx, al
+
+    ; 0x1f7 8bit  命令或状态端口
+    inc dx
+    mov al, 0x20
+    out dx, al
+
+; 验证状态
+; 3 0表示硬盘未准备好与主机交换数据 1表示准备好了
+; 7 0表示硬盘不忙 1表示硬盘忙
+; 0 0表示前一条指令正常执行 1表示执行出错 出错信息通过0x1f1端口获得
+.read_check:
+    mov dx, 0x1f7  ; 0x1f7 端口在写完 0x20 之后，就会返回硬盘状态，我们可以从这里读取硬盘状态
+    in al, dx      ; 将 0x1f7 端口的数据读入 al
+    and al, 0b10001000  ; 取硬盘状态的第3、7位
+    cmp al, 0b00001000  ; 当硬盘第 3 位是 1, 第 7 位是 0 时，表示硬盘数据准备好了不忙
+    jnz .read_check     ; 如果硬盘没有准备好，则跳转到 .read_check
+
+    ; 开始读数据
+    mov dx, 0x1f0     ; 硬盘的数据都是从这个端口读，一次读取 2 字节（16位）
+    mov cx, 256       ; 表示读取几次
+    mov edi, BOOT_MAIN_ADDR   ; 表示数据读到哪个地址
+
+.read_data:
+    in ax, dx         ; 把 dx 寄存器表示的端口的数据读入 ax
+    mov [edi], ax     ; 把 ax 寄存器的数据写入 edi 指向的地址
+    add edi, 2        ; 一次读取 2 字节，因此读完之后，目的往后移动 2 字节
+    ; 循环读取数据, 这个 loop 和 jmp 一样，都是跳转，但是 loop 会受 cx 的影响
+    ; loop 跳转之前会先判断 cx 是否为 0, 如果为 0 则跳转到 .read_data, 并且将 cx - 1
+    loop .read_data
+
+    mov dx, 0x1f7
+    in al, dx
+    and al, 0b00000001
+    cmp al, 0b00000001 ; 第 0 位为 1 表示有错误
+    jz .read_disk_error  ; 读取有误，跳转到 .read_disk_error
+
 
     ; 这里读取硬盘成功，打印跳转到 setup 的信息
     mov si, jmp_to_setup
