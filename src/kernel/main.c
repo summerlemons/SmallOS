13987e6c742d646aebbb1b415a30bb9887601b08
Martics <2032153705@qq.com>
2026-01-16 23:38:14 +0800

    features: 新增回车的解析; 提取获取当前光标位置的函数和设置当前光标位置的函数

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,29 +1,46 @@
 #include "../include/util/io.h"
 
+/**
+ * 获取光标位置
+ */
+unsigned short get_cursor_position() {
+    out_byte(0x3d4, 0x0E);
+    unsigned char cursor_position_high = in_byte(0x3d5);        // 获取光标高位
+
+    out_byte(0x3d4, 0x0F);
+    unsigned char cursor_position_low = in_byte(0x3d5);         // 获取光标低位
+    // 光标位置
+    return (cursor_position_high << 8) | cursor_position_low;   // 拼接
+}
+
+/**
+ * 设置光标位置
+ */
+void set_cursor_position(unsigned short position) {
+    out_byte(0x3d4, 0x0E);
+    out_byte(0x3d5, (char)(position >> 8));
+    out_byte(0x3d4, 0x0F);
+    out_byte(0x3d5, (char)(position));
+}
+
 void puts(char* str) {
+    // 光标位置
+    unsigned short cursor_position = get_cursor_position();
     // 遍历字符串
     char* p = str;
     while (*p != '\0') {
-        out_byte(0x3d4, 0x0E);
-        unsigned char cursor_position_high = in_byte(0x3d5);        // 获取光标高位
-
-        out_byte(0x3d4, 0x0F);
-        unsigned char cursor_position_low = in_byte(0x3d5);         // 获取光标低位
-        // 光标位置
-        unsigned short cursor_position = (cursor_position_high << 8) | cursor_position_low; // 拼接
-
-        // 光标位置打印字符
-        *(short*)(0xB8000 + cursor_position * 2) = *p | 0x0700; // 这里 0x0700 表示背景
+        if (*p == '\n') {
+            cursor_position = cursor_position + 80 - (cursor_position % 80); // 换行
+        } else {
+            // 光标位置打印字符
+            *(short*)(0xB8000 + cursor_position * 2) = *p | 0x0700; // 这里 0x0700 表示背景
+            cursor_position++;
+        }
+        
         // 字符串移动
         p++;
-
-        // 光标向后移动一位
-        cursor_position++;
-        out_byte(0x3d4, 0x0E);
-        out_byte(0x3d5, (char)(cursor_position >> 8));
-        out_byte(0x3d4, 0x0F);
-        out_byte(0x3d5, (char)(cursor_position));
     }
+    set_cursor_position(cursor_position);
 }
 
 // 编写内核主函数
@@ -44,6 +61,6 @@ void kernel_main(void) {
     out_byte(0x3d4, 0x0F);        // 标识设置光标位置 - 低位
     out_byte(0x3d5, 0x90);        // 组合起来就是设置光标在 0x190 位置，也就是第五行第一列
 
-    char* str = "We have finished the function of puts";
+    char* str = "We have finished the\nfunction of puts";
     puts(str);
 }
